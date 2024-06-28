ecb1db687a10f59bfe8eaf9aab037e8c341d864a
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2024-06-28 17:42:06 -0400

    Changed mapname to be the localization URL

--- src/graph/create_graph.py
+++ src/graph/create_graph.py
@@ -1,5 +1,5 @@
 # Create graph from a collection of intra-map graphs specified as CSV files.
-
+from pathlib import Path
 import glob
 
 import networkx as nx
@@ -38,11 +38,12 @@ def get_all_dfs_in_directory(directory):
     Given a directory, return a dictionary of all the dataframes
     in that directory, keyed by the name
     """
-    intramap_graph_csvs = glob.glob(f'{directory}/*.csv')
+    intramap_graph_csvs = glob.glob(f'{directory}/*/waypoints_graph.csv')
     mapname_to_df = {}
 
     for csv_path in intramap_graph_csvs:
-        mapname = '.'.join(csv_path.split('/')[-1].split('.')[:-1])
+        localization_url_file = Path(csv_path).parent / 'localization_url.txt'
+        mapname = localization_url_file.read_text().strip()
         this_map_df = pd.read_csv(csv_path, index_col = 'id')
         mapname_to_df[mapname] = this_map_df
     return mapname_to_df
@@ -54,4 +55,8 @@ def get_graph_and_dfs_from_directory(directory):
     """
     mapname_to_df = get_all_dfs_in_directory(directory)
     G = get_graph_from_df_list(list(mapname_to_df.values()))
+
+    print("Loaded graphs for URLs: ", list(mapname_to_df.keys()))
+    print(f"Graph has {len(G.nodes)} nodes and {len(G.edges)} edges")
+
     return G, mapname_to_df
