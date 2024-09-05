90dd1fd60e7edb6494ada29893befd0c434fda79
Sagar Bharadwaj <sagarbharadwaj50@gmail.com>
2024-09-04 22:18:44 -0400

    Updated debug text

--- src/client/src/utils/update-debug-text.ts
+++ src/client/src/utils/update-debug-text.ts
@@ -1,10 +1,17 @@
 function updateDebugText() {
     let debugText = "";
+    let activeServer = globalThis.discoveryObj.activeServer;
+    if (activeServer) {
+        let localizationData = activeServer.getLatestLocalizationData();
+        if (localizationData) {
+            debugText += `Active Server: ${localizationData.localizationID}: ${localizationData.serverConfidence}, ${localizationData.errorWithVIO}\n`;
+        }
+    }
     for (let mapServerName in globalThis.discoveryObj.mapServers) {
         let mapServer = globalThis.discoveryObj.mapServers[mapServerName];
         let localizationData = mapServer.getLatestLocalizationData();
         if (localizationData) {
-            debugText += `${mapServerName}: ${localizationData.serverConfidence}\n`;
+            debugText += `${mapServerName}: ${localizationData.localizationID}: ${localizationData.serverConfidence}, ${localizationData.errorWithVIO}\n`;
         }
     }
     globalThis.debugEl.setAttribute('value', debugText);
